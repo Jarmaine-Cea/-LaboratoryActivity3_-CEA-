182f323a0e201020f2d0e0e8ea69518fa09acd1b
Jarmaine-Cea <jarmainemouelgomez.cea24@bicol-u.edu.ph>
2025-09-21 17:32:52 +0800

    Final

---
+++ MultiplicationTable.js
@@ -0,0 +1,9 @@
+// Problem 4: Multiplication Table
+
+function multiplicationTable(n) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${n} x ${i} = ${n * i}`);
+    }
+}
+
+multiplicationTable(5);
